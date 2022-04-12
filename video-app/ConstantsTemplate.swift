3c0e0162488f9b7df4ab6fad849aef35511cd416
Fabio Menozzi <f.menozzi@yahoo.com>
2022-04-12 12:31:20 -0300

    modified gitignore to do not track the class Constants with keys, and created ConstantTemplate

---
+++ video-app/ConstantsTemplate.swift
@@ -0,0 +1,22 @@
+//
+//  ConstantsTemplate.swift
+//  video-app
+//
+//  Created by Fabio Menozzi on 12/04/22.
+//
+
+
+
+/*
+ Add your own keys below and uncomment the code
+ */
+
+import Foundation
+
+//struct Constants {
+//
+//    static var API_KEY = ""
+//    static var PLAYLIST_ID = ""
+//    static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlists?part=snippet&id=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+//
+//}
---
+++ video-app/Video.swift
@@ -0,0 +1,16 @@
+//
+//  Video.swift
+//  video-app
+//
+//  Created by Fabio Menozzi on 29/03/22.
+//
+
+import Foundation
+
+struct Video {
+    var videoId = ""
+    var title = ""
+    var description = ""
+    var thumbnail = ""
+    var published = ""
+}
